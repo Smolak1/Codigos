1aeb9c5df0094fbf1e5d713d9c83e28e68d13bf4
Smolak1 <jcsmolakr@gmail.com>
2025-03-18 15:39:46 -0400

    aquivo adicionado

---
+++ teste.js
@@ -0,0 +1,8 @@
+function par(numero){
+    let result = numero % 2
+    if (result == 0){
+        console.log("O número é PAR")
+}           else console.log("O número é IMPAR")
+}
+
+par(110000000001)
