b4b9704fa6610dfa76bca4ac04dddbddbf537547
Smolak1 <jcsmolakr@gmail.com>
2025-03-18 15:11:38 -0400

    Calculo combustivel

---
+++ index.js
@@ -0,0 +1,4 @@
+function Calculo(kmLitro, distancia, preco){
+    gasto = (distancia / kmLitro) * preco
+        console.log("O gasto total foi de " + gasto.toFixed(3));};
+        Calculo(10, 16, 6.89);
