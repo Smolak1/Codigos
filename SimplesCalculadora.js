afd4178ae2f7e3f8c241fb43edc0560084f22752
Smolak1 <jcsmolakr@gmail.com>
2025-03-19 16:56:15 -0400

    Fataração

--- SimplesCalculadora.js
+++ SimplesCalculadora.js
@@ -1,11 +1,8 @@
 function Calcular(n1, op, n2){
     
-    if(op === "*"){
+    if(op === "*" || op === "x"){
         result = n1 * n2
         console.log(`O resultado da expressão ${n1} ${op} ${n2} foi ${result}.`)
-    }else if(op === "x"){
-            result = n1 * n2
-            console.log(`O resultado da expressão ${n1} ${op} ${n2} foi ${result}.`)
         }else if(op === "-"){
             result = n1 - n2
             console.log(`O resultado da expressão ${n1} ${op} ${n2} foi ${result}.`)}
@@ -19,6 +16,17 @@ function Calcular(n1, op, n2){
                         result = n1 % n2
                         console.log(`O resultado da expressão ${n1} ${op} ${n2} foi ${result}.`)
                         }
-    }
+                    }
 
-Calcular(1, "+", 1)
+function fatoracao(n1){
+let fatoriado = 1n 
+while(n1 > 1){
+    fatoriado *= BigInt(n1);
+        console.log(`Fatorando...${fatoriado}`)
+        n1--       
+
+
+}
+}
+                    fatoracao()
+                    Calcular(10, "x", 2)
